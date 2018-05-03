ef91baf91d5b32d0d185c13d13ea5226a92dc5c6
tuvitech <tuvvik.hk@gmail.com>
2018-05-03 21:24:20 +0530

    email

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php
+$to = "tuvvik.hk@gmail.com";
+$subject = "Hi!";
+$body = "Hi,\n\nHow are you?";
+if (mail($to, $subject, $body)) {
+echo("<p>Email successfully sent!</p>");
+} else {
+echo("<p>Email delivery failed…</p>");
+}
+?>

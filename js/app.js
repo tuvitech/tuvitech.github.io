b2d6c8dfb800c8af436eb12821dd6a8513e0a9bd
tuvitech <tuvvik.hk@gmail.com>
2018-05-04 01:39:02 +0530

    modified style

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
-// $(document).ready(function(){
-//   $(#sendbtn).click(function(){
-//     $.ajax
-//   });
-// });
+$(document).ready(function(){
+  //Disable cut copy paste
+  // $('body').on('cut copy paste', function (e) {
+  //     e.preventDefault();
+  // });
+  //
+  // //Disable mouse right click
+  // $("body").on("contextmenu",function(e){
+  //     return false;
+  // });
+
+  $("#write-header").addClass("swing animated");
+  $("#sendbtn").addClass("bounceInUp animated");
+});
